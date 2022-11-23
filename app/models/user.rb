229ac7c9069c7d2f8241e89b2b05f035ca39b301
crisandryj <crisandryj@gmail.com>
2022-11-23 08:46:04 -0500

    friend model created"

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :comments
+  has_many :posts
+
+  has_many :friends,class_name: "User",
+                    foreign_key: "friend_id"
+
+  belongs_to :friend, class_name: "User"
 end
