d9f9f0bd26116a2634548b235ad0ecfe097cf1f7
crisandryj <crisandryj@gmail.com>
2022-11-23 14:28:16 -0500

    created user to user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
 
-  has_many :friends,class_name: "User",
-                    foreign_key: "friend_id"
+  has_many :friend_followers, foreign_key: :followee_id, class_name: "Friend"
+  has_many :followers, through: :friend_followers, source: :follower
 
-  belongs_to :friend, class_name: "User"
+  has_many :friend_followees, foreign_key: :follower_id, class_name: "Friend"
+  has_many :followees, through: :friend_follower, source: :followee
+
 end
