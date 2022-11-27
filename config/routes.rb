788c8c860d3f6cf12dc1ec974d3441dd0c521690
crisandryj <crisandryj@gmail.com>
2022-11-27 15:52:40 -0500

    created request controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'requests/index'
+  get 'requests/new'
+  get 'requests/create'
+  get 'requests/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
