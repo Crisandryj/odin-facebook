e5ca270fdc26342f1598215ba428f4a3942cf858
crisandryj <crisandryj@gmail.com>
2022-11-28 13:38:37 -0500

    updated associations

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,13 +1,29 @@
 class RequestsController < ApplicationController
   def index
+    @request = Request.all
   end
 
   def new
+    @request = Request.new
   end
 
   def create
+    @request = Request.create(request_params)
+    if @request.save
+      flash[:notice] = "Sent friend request!"
+    else
+      render 'new'
+    end
   end
 
   def destroy
   end
+
+
+  private
+
+  def request_params
+    params.require(:request).permit(:accept)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,15 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
 
-  has_many :friend_follows, foreign_key: :followee_id, class_name: "Friend"
+  has_many :friend_follows, foreign_key: :followee_id, class_name: 'Friend'
   has_many :followers, through: :friend_follows
 
-  has_many :friend_followees, foreign_key: :follower_id, class_name: "Friend"
+  has_many :friend_followees, foreign_key: :follower_id, class_name: 'Friend'
   has_many :followees, through: :friend_followees
 
-  has_many :recieved_requests, foreign_key: :invitee_id, class_name: "Request"
+  has_many :recieved_requests, foreign_key: :invitee_id, class_name: 'Request'
   has_many :invitors, through: :recieved_requests
 
-  has_many :sent_requests, foreign_key: :invitor_id, class_name: "Request"
+  has_many :sent_requests, foreign_key: :invitor_id, class_name: 'Request'
   has_many :invitees, through: :sent_requests
-
-
-
-
 end
