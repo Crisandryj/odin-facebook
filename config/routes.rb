78283fcdb83c24f18ea1f2b938107f3cb86e9ea2
crisandryj <crisandryj@gmail.com>
2022-12-02 08:17:07 -0500

    created form for post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'posts/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   resources :users
   resources :friends
   resources :requests
+  resources :posts
 end
