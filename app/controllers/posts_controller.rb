4f8b355744edead0999b35363bdf781b365c9003
crisandryj <crisandryj@gmail.com>
2022-12-02 08:31:46 -0500

    displaying error messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
-  def new
+  def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash.now[:notice] = 'posted'
+    else
+      flash[:notice] = @post.errors.full_messages
+      render 'new'
+    end
+  end
+
+
+  private
+
+    def post_params
+      params.require(:post).permit(:body)
+    end
+
 end
