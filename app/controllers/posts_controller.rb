52192748241e87a3002b90d05379fbb43893b950
crisandryj <crisandryj@gmail.com>
2022-12-02 08:50:02 -0500

    user can post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:body)
+      params.require(:post).permit(:body,:user_id)
     end
 
 end
