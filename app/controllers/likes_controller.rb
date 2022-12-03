a95a741384fb23b0fba4d9c00133000505ef1151
crisandryj <crisandryj@gmail.com>
2022-12-03 11:47:01 -0500

    new and create methods for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @like = Like.create(like_params)
+  end
+
+
+  private
+
+  def like_params
+    params.require(:like).permit(:like)
+  end
 end
