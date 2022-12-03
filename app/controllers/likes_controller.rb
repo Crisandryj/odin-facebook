003388e220c909a7096734de253bf25e8db0326f
crisandryj <crisandryj@gmail.com>
2022-12-03 13:04:03 -0500

    ability to like post added

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,12 +5,22 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(like_params)
+    if @like.save
+      flash.now[:notice] = "liked post"
+    else
+      flash[:notice] = @like.errors.full_messages
+      render new
+    end
   end
 
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
+  end
 
   private
 
   def like_params
-    params.require(:like).permit(:like)
+    params.permit(:user_id,:post_id,:like)
   end
 end
