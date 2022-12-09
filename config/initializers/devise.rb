e83b2db68caa7c36ef68cf4f5a3a730df69eb191
crisandryj <crisandryj@gmail.com>
2022-12-09 08:50:09 -0500

    sign in with facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.omniauth :facebook, "527059162666115", "3dddc7b5d35f6d3300791b0721785f82"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
